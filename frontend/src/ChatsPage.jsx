19ecbeadd4a8071d42cae3faccfa6ca784e3b8fb
Recep Enes <recepenskaya@gmail.com>
2023-10-24 14:27:35 +0300

    Sunucu bulunamadı
    Uncaught ReferenceError
    <ChatsPage>
    Uncaught ReferenceError
    Yapacaz Bisiler

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,21 @@
+import { PrettyChatWindow} from 'react-chat-engine-pretty'
 const ChatsPage = () => {
-    return <>chats...</>
+    const chatProps = useMultiChatLogic(
+        '95ecb2c2-d330-4426-a08d-89b09db19a97',
+         props.user.username,
+         props.user.secret
+    );
+    return (
+         <div style={{ height:'100vh'}}>
+               <PrettyChatWindow
+                    projectId='95ecb2c2-d330-4426-a08d-89b09db19a97'
+                    username={props.user.username}
+                    secret={props.user.secret}
+                    style={{height: '100%'}}
+                />
+         </div>
+                    
+    )
 }
 
 export default ChatsPage
